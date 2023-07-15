8a2ae406fd51ce3e8e54c13e6f8c259bd97be91e
3Melody <plugeiei2003@gmail.com>
2023-07-15 16:01:26 +0700

    day 4 part-1

--- script.js
+++ script.js
@@ -11,4 +11,45 @@ function tabbar(){
     }
 }
 
+var slides = document.querySelectorAll('.slide');
+var dots = document.querySelectorAll('.dot');
+var currentSlide = 0;
+var slideInterval;
 
+function startSlide() {
+  slideInterval = setInterval(nextSlide, 8000);
+}
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slides[currentSlide].style.display = 'none';
+  dots[currentSlide].classList.remove('active');
+  currentSlide = (n + slides.length) % slides.length;
+  slides[currentSlide].style.display = 'block';
+  dots[currentSlide].classList.add('active');
+}
+
+var nextBtn = document.querySelector('.next');
+nextBtn.addEventListener('click', nextSlide);
+
+var prevBtn = document.querySelector('.prev');
+prevBtn.addEventListener('click', prevSlide);
+
+for (var i = 0; i < dots.length; i++) {
+  dots[i].addEventListener('click', function() {
+    var clickedIndex = Array.from(dots).indexOf(this);
+    goToSlide(clickedIndex);
+  });
+}
+
+window.onload = function() {
+  slides[currentSlide].style.display = 'block';
+  dots[currentSlide].classList.add('active');
+};
